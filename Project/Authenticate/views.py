5537aa10ef79ab01a47f7eeca125d2b421e63792
Walobwa_D <walobwadan@gmail.com>
2022-10-07 11:57:22 +0300

    Styled the landing page

--- Project/Authenticate/views.py
+++ Project/Authenticate/views.py
@@ -15,33 +15,33 @@ from .serializer import *
 def register(request):
     registered = False
     user_form = UserForm()
-    profile_form = UserProfileForm()
+    # profile_form = UserProfileForm()
     if request.method == 'POST':
         user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
+        #profile_form = UserProfileForm(request.POST)
         
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid(): #and profile_form.is_valid():
             
             user = user_form.save()
             user.set_password(user.password)
             user.save()
             
-            profile = profile_form.save(commit = False)
-            profile.user = user
+            #profile = profile_form.save(commit = False)
+            #profile.user = user
             
-            if 'profilepic' in request.FILES:
-                profile.profilepic = request.FILES['profilepic']
+            # if 'profilepic' in request.FILES:
+            #     profile.profilepic = request.FILES['profilepic']
                 
-            profile.save()
+            # profile.save()
             
             registered = True
     else:
         user_form = UserForm()
-        profile_form = UserProfileForm()          
+        #profile_form = UserProfileForm()          
         
     context = {
         'user_form' : user_form,
-        'profile_form' : profile_form
+        #'profile_form' : profile_form
     }  
     return render(request, 'Authenticate/register.html', context)
 
